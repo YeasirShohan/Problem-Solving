381b236878bec7e6dfa19e1ac9367294e57a2c4a
Yeasir Arafat <105309268+YeasirShohan@users.noreply.github.com>
2024-03-17 23:46:39 +0600

    Add files via upload

---
+++ SuperAIprogram.py
@@ -0,0 +1,31 @@
+import time
+
+print("AI: What is your Name?")
+name1 = input("You: ")
+
+print("AI: Where are you from?")
+name2 = input("You: ")
+
+print("AI: What is your Profession?")
+name3 = input("You: ")
+
+print("AI: What is your hobby?")
+name4 = input("You: ")
+print(" ")
+
+print("..")
+time.sleep(2)
+print("...")
+time.sleep(3)
+print("....")
+time.sleep(3)
+
+print(" ")
+print(''' "AI:Hello sir,
+thanks for giving me your informetion.''')
+print("Your name is",name1)
+print("You are from",name2)
+print("your profession is",name3)
+print("And Your hobby is",name4)
+print("enjoy your",name3)
+print("life and enjoy your",name4)
