143a19b6c97a510869d1e62301985f8ec8290708
Yeasir Arafat <105309268+YeasirShohan@users.noreply.github.com>
2024-12-20 23:39:41 +0600

    Update Plus Minus.py

--- Plus Minus.py
+++ Plus Minus.py
@@ -1,3 +1,11 @@
+'''
+Question:"Plus Minus"
+Given an array of integers, calculate the ratios of its elements that are positive, 
+negative, and zero. Print the decimal value of each fraction on a new line with  places 
+after the decimal.
+'''
+
+#Solution
 def plusMinus(arr):
     pval=0
     nval=0
