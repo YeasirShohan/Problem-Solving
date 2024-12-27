a765da28a10255c3d4879e1c1ea776dbaf100855
Yeasir Arafat <105309268+YeasirShohan@users.noreply.github.com>
2024-12-27 10:45:59 +0600

    Update lonelyinteger.py

--- lonelyinteger.py
+++ lonelyinteger.py
@@ -1,3 +1,8 @@
+'''
+Question:
+Given an array of integers, where all elements but one occur twice, find the unique element.
+Note: This problem also can solved in better option. which is "Bit Manipulation"
+'''
 def lonelyinteger(a):
     unique=[]
     for i in a:
